2d5102d4160e11ffa1066c103b6d57b799b112a4
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-12 16:14:32 -0600

    :memo: upadate document

--- src/Components/Stack.jsx
+++ src/Components/Stack.jsx
@@ -1,31 +1,41 @@
-import "./stacks.css";
+import React from "react";
 import { motion } from "framer-motion";
+import "./stacks.css";
 
-export default () => {
+const Stack = () => {
+  const skills = {
+    frontEnd: [
+      { src: "/images/React-Dark.svg", alt: "React" },
+      { src: "/images/NextJS-Dark.svg", alt: "Next.js" },
+      // Agrega más habilidades de Front-End aquí
+    ],
+    backEnd: [
+      { src: "/images/NodeJS-Dark.svg", alt: "Node.js" },
+      { src: "/images/Docker.svg", alt: "Docker.js" },
+      // Agrega más habilidades de Back-End aquí
+    ],
+    utilities: [
+      { src: "/images/VSCode-Dark.svg", alt: "VS Code" },
+      { src: "/images/Vite-Dark.svg", alt: "Vite" },
+      // Agrega más utilidades aquí
+    ],
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="page__container"
+      className="page__container -z-0 "
     >
       <div className="page__wrapper">
         <div className="page__hero">
           <div className="page__image"></div>
           <div className="page__text"></div>
           <section className="stack relative h-full w-full">
-            <header className="fixed">
-              <i
-                className="bx bx-mouse fixed text-black/60  text-8xl bottom-0 left-0 translate-x-8
-                hover:scale-90 transition-all duration-300 hover:text-blue-600 hover:hue-rotate-30
-                cursor-zoom-in hover:row-end-auto hover:transition-transform hover:rotate-[360deg] hover:ring-offset-teal-400 p-2 
-              "
-              ></i>
-            </header>
             <h2 className="text-center text-black text-[3.2rem] font-bold p-5">
               Mis habilidades como desarrollador
             </h2>
-
             <div className="relative w-full">
               <p className="color-animating-text">
                 Haz <span className="text-5xl">scroll</span> aquí
@@ -33,203 +43,49 @@ export default () => {
               </p>
             </div>
 
-            <h3 className="text-left text-6xl font-bold p-6 text-black ">
+            <h3 className="text-left text-6xl font-bold p-6 text-black">
               Front-End
             </h3>
-
-            <section className=" grid grid-cols-2 gap-4 mt-8 ">
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/React-Dark.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/NextJS-Dark.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Redux.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/HTML.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/CSS.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/JavaScript.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/TailwindCSS-Dark.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Sass.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Bootstrap.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/MaterialUI-Light.svg"
-                  alt=""
-                />
-              </div>
+            <section className="grid grid-cols-2 gap-4 mt-8">
+              {skills.frontEnd.map((skill, index) => (
+                <div key={index}>
+                  <img
+                    className="imgtec mx-auto w-[11rem] object-contain h-[10rem]"
+                    src={skill.src}
+                    alt={skill.alt}
+                  />
+                </div>
+              ))}
             </section>
 
-            <h3 className="text-center text-6xl font-bold p-6 text-black ">
+            <h3 className="text-center text-6xl font-bold p-6 text-black">
               Back-End
             </h3>
-
-            <section className=" grid grid-cols-2 gap-4 mt-8 ">
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/NodeJS-Dark.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/ExpressJS-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Docker.svg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/PostgreSQL-Dark.svg"
-                  alt=""
-                />
-              </div>
+            <section className="grid grid-cols-2 gap-4 mt-8">
+              {skills.backEnd.map((skill, index) => (
+                <div key={index}>
+                  <img
+                    className="imgtec mx-auto w-[11rem] object-contain h-[10rem]"
+                    src={skill.src}
+                    alt={skill.alt}
+                  />
+                </div>
+              ))}
             </section>
 
-            <h3 className="text-center text-5xl font-bold p-6 text-black ">
+            <h3 className="text-center text-5xl font-bold p-6 text-black">
               Algunas de mis utilidades
             </h3>
-
-            <section className=" grid grid-cols-2 gap-4 mt-8">
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/VSCode-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Vite-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Firebase-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Discord.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/StackOverflow-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/Netlify-Dark.svg"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/termianal.png"
-                  alt=""
-                />
-              </div>
-
-              <div>
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/git.png"
-                  alt=""
-                />
-              </div>
-
-              <div className="mb-10">
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/notion.png"
-                  alt=""
-                />
-              </div>
-
-              <div className="mb-10">
-                <img
-                  className=" imgtec mx-auto w-[11rem] object-contain h-[10rem]  "
-                  src="/images/figma.png"
-                  alt=""
-                />
-              </div>
+            <section className="grid grid-cols-2 gap-4 mt-8">
+              {skills.utilities.map((skill, index) => (
+                <div key={index}>
+                  <img
+                    className="imgtec mx-auto w-[11rem] object-contain h-[10rem]"
+                    src={skill.src}
+                    alt={skill.alt}
+                  />
+                </div>
+              ))}
             </section>
           </section>
         </div>
@@ -237,3 +93,5 @@ export default () => {
     </motion.div>
   );
 };
+
+export default Stack;
