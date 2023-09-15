e3cc116b642245ef22c3f3164ef5b5ca5c466d13
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-15 17:38:53 -0600

    :construction: Work in progress

---
+++ src/pages/certifecad.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const certifecad = () => {
+  return (
+    <div>
+      newApp
+      <h1>Mis certificados como proggramador web</h1>
+    </div>
+  );
+};
+
+export default certifecad;
